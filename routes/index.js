1c9f9f0f90046d178e81e656afaf7418c17a78c6
Jennifer Brown <j.a.brown.5@gmail.com>
2017-03-30 16:00:41 -0500

    basics have been set up

--- routes/index.js
+++ routes/index.js
@@ -6,33 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/about', function(req, res, next) {
-    res.render('about', { });
-});
-
-router.get('/contact', function(req, res, next) {
-    res.render('contact', { });
-});
-
-router.get('/causes', function(req, res, next) {
-    res.render('causes', { });
-});
-
-router.get('/data', function(req, res, next) {
-    res.render('data', { });
-});
-
-router.get('/coding', function(req, res, next) {
-    res.render('coding', { });
-});
-
-router.get('/blog', function(req, res, next) {
-    res.render('blog', { });
-});
-
-router.get('/good-cat', function(req, res, next) {
-    res.render('good-cat-game', { });
-});
 
 router.get('/divvy', function(req, res, next) {
     res.render('divvy', { });
@@ -48,18 +21,20 @@ router.get('/ga-job-fair', function(req, res, next) {
 
 
 
-router.get('/nfl-arrests', function(req, res, next) {
-    res.render('nfl-arrests', { });
-});
-
-router.get('/potty-check', function(req, res, next) {
-    res.render('potty-check', { });
-});
-
-router.get('/question-game', function(req, res, next) {
-    res.render('question-game', { });
+router.get('/nfl-arrests-d3', function(req, res, next) {
+    res.render('nfl-arrests-d3', { });
 });
 
 
+
+
+router.get('/test', function(req, res, next) {
+    res.render('test', { });
+});
+
+
+router.get('/data.tsv', function(req, res, next) {
+    res.render('data.tsv', { });
+});
 
 module.exports = router;
